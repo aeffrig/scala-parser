c5e09631b7da54130da6125c6ede937e57802233
Paul Phillips <paulp@improving.org>
2014-11-29 20:52:29 -0800

    Clean up test.

--- src/test/scala/scalaParser/SyntaxTest.scala
+++ src/test/scala/scalaParser/SyntaxTest.scala
@@ -28,6 +28,7 @@ object SyntaxTest {
   def checkNeg[T](input: String) = {
     new ScalaSyntax(input).CompilationUnit.run() match {
       case Failure(f: ParseError) => () // yay
+      case Failure(f)             => runtimeException(s"Unexpected failure: $f")
       case Success(parsed)        => assert(parsed != input, parsed)
     }
   }
@@ -42,35 +43,52 @@ object SyntaxTest {
       case Success(`input`)       => ()
       case Success(parsed)        => runtimeException(snippet(input, parsed))
       case Failure(f: ParseError) => runtimeException(f.position + "\t" + f.formatTraces)
+      case Failure(f)             => runtimeException(s"Unexpected failure: $f")
     }
   }
-  def main(args: Array[String]): Unit = {
-    val failures = Vector.newBuilder[Path]
-    scalaPaths foreach { f =>
-      val input = f.slurp()
-      val fs = f.toString
-      val segments = (fs splitChar '/').toSet
-      val path_s = fs stripPrefix (scalaSources + "/") match {
-        case s if s.length <= maxFileLen => s
-        case s                           => "..." + s.substring(s.length - (maxFileLen - 3), s.length)
+
+  def checkPath(f: Path): Boolean = {
+    val input = f.slurp()
+    val fs = f.toString
+    val segments = (fs splitChar '/').toSet
+    val path_s = fs stripPrefix (scalaSources + "/") match {
+      case s if s.length <= maxFileLen => s
+      case s                           => "..." + s.substring(s.length - (maxFileLen - 3), s.length)
+    }
+    val isNeg  = segments("neg")
+    val isSkip = (skipPaths exists fs.endsWith) || segments("disabled") || segments("pending") || segments("script")
+    val fmt    = s"[%6s] $maxFileFmt  "
+
+    print((s"[%6s] $maxFileFmt  ").format(input.length, path_s))
+
+    if (isSkip) {
+      println("skip".yellow.to_s)
+      true
+    }
+    else {
+      val result = Try(check(input))
+      val passed = result match {
+        case Success(_) => !isNeg
+        case Failure(_) => isNeg
       }
-      val isNeg = segments("neg")
-      val skip  = (skipPaths exists fs.endsWith) || segments("disabled") || segments("pending") || segments("script")
-      val fmt   = s"[%6s] $maxFileFmt  "
-      print((s"[%6s] $maxFileFmt  ").format(input.length, path_s))
       println(
-        if (skip) "skip".yellow.to_s
-        else Try(check(input)) match {
-          case Success(_) if isNeg => failures += f ; "failed".red.to_s + " (expected failure)"
-          case Success(_)          => "ok".green.to_s
-          case Failure(_) if isNeg => "ok".green.to_s
-          case Failure(t)          => failures += f ; "failed".red.to_s + "\n" + t.getMessage
+        if (passed) "ok".green.to_s
+        else result match {
+          case Success(_) => "failed".red.to_s + " (expected failure)"
+          case Failure(t) => "failed".red.to_s + "\n" + t.getMessage
         }
       )
+      passed
     }
-    println("\nFailures:")
-    val failed = failures.result
-    failed foreach println
-    assert(failed.isEmpty, "There were test failures.")
+  }
+
+  def main(args: Array[String]): Unit = {
+    scalaPaths filterNot checkPath match {
+      case Seq()    =>
+      case failures =>
+        println("\nFailures:")
+        failures foreach println
+        runtimeException("There were test failures.")
+    }
   }
 }
